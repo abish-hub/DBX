5156fa381a82a872e01655af1c26dac98c847573
abish-hub <abishrajav@gmail.com>
2026-01-11 16:08:18 +0530

    Update stg_example.sql

--- models/stg_example.sql
+++ models/stg_example.sql
@@ -1,1 +1,29 @@
 
+{{ config(
+    catalog = "azure_blob",
+    schema='analytics',      -- Explicit schema override (optional)
+    materialized='table'
+) }}
+
+WITH base AS (
+    SELECT
+        customer_id,
+        first_name,
+        last_name,
+        email_address,
+        modified_date,
+        company_name
+    FROM {{ source('raw', 'customer_output') }}  -- raw schema from Fivetran
+)
+
+SELECT
+    customer_id,
+    first_name,
+    last_name,
+    email_address,
+    company_name,
+    modified_date,
+    ROW_NUMBER() OVER (PARTITION BY company_name ORDER BY modified_date DESC) AS company_rank,
+    COUNT(*) OVER (PARTITION BY company_name) AS total_customers_in_company
+FROM base
+
